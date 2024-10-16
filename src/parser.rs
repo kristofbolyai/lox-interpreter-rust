c635ef38fb1feaf0e00ac8c76a2ed10c50206a4b
Kristof <49001742+kristofbolyai@users.noreply.github.com>
2024-10-16 08:42:31 +0200

    refactor: refactor towards single-or-double char tokens

--- src/parser.rs
+++ src/parser.rs
@@ -2,20 +2,25 @@ pub mod parser {
     use std::fmt::{Display, Formatter};
     use std::iter::Peekable;
     use std::str::Chars;
+    use convert_case::{Case, Casing};
+    use crate::parser::parser::TokenLength::{Eof, Single, SingleOrDouble};
 
     macro_rules! create_token {
-        ($token_type:expr, $lexem:expr) => {
+        ($token_type:expr) => {
             Token {
                 token_type: $token_type,
-                lexem: $lexem,
+                lexem: $token_type.token_lexem().to_string(),
             }
         };
     }
 
     #[derive(Debug)]
-    pub struct Token {
-        pub token_type: TokenType,
-        pub lexem: String,
+    enum TokenLength {
+        Single,
+        SingleOrDouble,
+        Literal,
+        Keyword,
+        Eof
     }
 
     #[derive(Debug)]
@@ -33,27 +38,87 @@ pub mod parser {
         Slash,
         Star,
 
+        // Single or double character,
+        Bang,
+        BangEqual,
+        Equal,
+        EqualEqual,
+        Greater,
+        GreaterEqual,
+        Less,
+        LessEqual,
+
         // Special
         EOF,
+    }
+
+    impl TokenType {
+        fn token_length(&self) -> TokenLength {
+            match &self {
+                TokenType::LeftParenthesis => Single,
+                TokenType::RightParenthesis => Single,
+                TokenType::LeftBrace => Single,
+                TokenType::RightBrace => Single,
+                TokenType::Comma => Single,
+                TokenType::Dot => Single,
+                TokenType::Minus => Single,
+                TokenType::Plus => Single,
+                TokenType::Semicolon => Single,
+                TokenType::Slash => Single,
+                TokenType::Star => Single,
+                TokenType::Bang => SingleOrDouble,
+                TokenType::BangEqual => SingleOrDouble,
+                TokenType::Equal => SingleOrDouble,
+                TokenType::EqualEqual => SingleOrDouble,
+                TokenType::Greater => SingleOrDouble,
+                TokenType::GreaterEqual => SingleOrDouble,
+                TokenType::Less => SingleOrDouble,
+                TokenType::LessEqual => SingleOrDouble,
+                TokenType::EOF => Eof
+            }
+        }
+
+        fn token_lexem(&self) -> &str {
+            match &self {
+                TokenType::LeftParenthesis => "(",
+                TokenType::RightParenthesis => ")",
+                TokenType::LeftBrace => "{",
+                TokenType::RightBrace => "}",
+                TokenType::Comma => ",",
+                TokenType::Dot => ".",
+                TokenType::Minus => "-",
+                TokenType::Plus => "+",
+                TokenType::Semicolon => ";",
+                TokenType::Slash => "/",
+                TokenType::Star => "*",
+                TokenType::Bang => "!",
+                TokenType::BangEqual => "!=",
+                TokenType::Equal => "=",
+                TokenType::EqualEqual => "==",
+                TokenType::Greater => ">",
+                TokenType::GreaterEqual => ">=",
+                TokenType::Less => "<",
+                TokenType::LessEqual => "<=",
+                TokenType::EOF => ""
+            }
+        }
     }
 
     impl Display for TokenType {
         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
             match &self {
+                // Non-default cases
                 TokenType::LeftParenthesis => write!(f, "LEFT_PAREN"),
                 TokenType::RightParenthesis => write!(f, "RIGHT_PAREN"),
-                TokenType::LeftBrace => write!(f, "LEFT_BRACE"),
-                TokenType::RightBrace => write!(f, "RIGHT_BRACE"),
-                TokenType::Comma => write!(f, "COMMA"),
-                TokenType::Dot => write!(f, "DOT"),
-                TokenType::Minus => write!(f, "MINUS"),
-                TokenType::Plus => write!(f, "PLUS"),
-                TokenType::Semicolon => write!(f, "SEMICOLON"),
-                TokenType::Slash => write!(f, "SLASH"),
-                TokenType::Star => write!(f, "STAR"),
-                TokenType::EOF => write!(f, "EOF"),
+                _ => write!(f, "{}", format!("{:?}", self).to_case(Case::UpperSnake)),
             }
         }
+    }
+
+    #[derive(Debug)]
+    pub struct Token {
+        pub token_type: TokenType,
+        pub lexem: String,
     }
 
     pub enum ParseError {
@@ -96,16 +161,21 @@ pub mod parser {
 
             let mut char_iter = &mut self.source_code;
 
-            // FIXME: Count lines
-            let line: u64 = 1;
+            let mut line: u64 = 1;
 
             loop {
-                if let Some(token) = Self::try_parse_single_character_token(&mut char_iter) {
-                    char_iter.next();
+                if let Some(token) = Self::try_parse_single_or_double_character_token(&mut char_iter) {
+                    tokens.push(token);
+                } else if let Some(token) = Self::try_parse_single_character_token(&mut char_iter) {
                     tokens.push(token);
                 } else {
                     // Handle unexpected characters
                     if let Some(char) = char_iter.next() {
+                        if char == '\n' {
+                            line += 1;
+                            continue;
+                        }
+
                         errors.push(ParseError::UnexpectedCharacter {
                             character: char.to_string(),
                             line,
@@ -117,28 +187,39 @@ pub mod parser {
                 }
             }
 
-            tokens.push(create_token!(TokenType::EOF, String::new()));
+            tokens.push(create_token!(TokenType::EOF));
 
             ParseResult { tokens, errors }
         }
 
+        // FIXME: Try to avoid memory allocations from to_string
+        fn try_parse_single_or_double_character_token(char_iter: &mut Peekable<Chars>) -> Option<Token> {
+            None
+        }
+
+        // FIXME: Try to avoid memory allocations from to_string
         fn try_parse_single_character_token(char_iter: &mut Peekable<Chars>) -> Option<Token> {
             if let Some(char) = char_iter.peek() {
-                // FIXME: Try to avoid memory allocations from to_string
-                return match char {
-                    '(' => Some(create_token!(TokenType::LeftParenthesis, char.to_string())),
-                    ')' => Some(create_token!(TokenType::RightParenthesis, char.to_string())),
-                    '{' => Some(create_token!(TokenType::LeftBrace, char.to_string())),
-                    '}' => Some(create_token!(TokenType::RightBrace, char.to_string())),
-                    ',' => Some(create_token!(TokenType::Comma, char.to_string())),
-                    '.' => Some(create_token!(TokenType::Dot, char.to_string())),
-                    '-' => Some(create_token!(TokenType::Minus, char.to_string())),
-                    '+' => Some(create_token!(TokenType::Plus, char.to_string())),
-                    ';' => Some(create_token!(TokenType::Semicolon, char.to_string())),
-                    '/' => Some(create_token!(TokenType::Slash, char.to_string())),
-                    '*' => Some(create_token!(TokenType::Star, char.to_string())),
+                let token = match char {
+                    '(' => Some(create_token!(TokenType::LeftParenthesis)),
+                    ')' => Some(create_token!(TokenType::RightParenthesis)),
+                    '{' => Some(create_token!(TokenType::LeftBrace)),
+                    '}' => Some(create_token!(TokenType::RightBrace)),
+                    ',' => Some(create_token!(TokenType::Comma)),
+                    '.' => Some(create_token!(TokenType::Dot)),
+                    '-' => Some(create_token!(TokenType::Minus)),
+                    '+' => Some(create_token!(TokenType::Plus)),
+                    ';' => Some(create_token!(TokenType::Semicolon)),
+                    '/' => Some(create_token!(TokenType::Slash)),
+                    '*' => Some(create_token!(TokenType::Star)),
                     _ => None,
                 };
+
+                if let Some(_) = token {
+                    char_iter.next();
+                }
+
+                return token;
             }
 
             None
